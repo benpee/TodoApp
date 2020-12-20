c18c48f60b22b03309ff0fe13629392e760ddaf0
benpee <74276700+benpee@users.noreply.github.com>
2020-12-20 06:03:25 +0100

    Add files via upload

---
+++ Todo App/precache-manifest.9460927cca2cb51c2e0ea9ad5b88243c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fdb8907d7f5c05362c10e083e59369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d88143899cf38d16b217",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "d1be5d36e7c9e0d51b58",
+    "url": "/static/css/main.a75f2e9e.chunk.css"
+  },
+  {
+    "revision": "d88143899cf38d16b217",
+    "url": "/static/js/2.78802be3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78802be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1be5d36e7c9e0d51b58",
+    "url": "/static/js/main.335a039f.chunk.js"
+  },
+  {
+    "revision": "2ae6e346d186b5e5b788",
+    "url": "/static/js/runtime-main.ca8a46de.js"
+  }
+]);
